ee9ce413657659c912531ab7c0742bb6d59f0f13
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-25 12:08:51 -0600

    Start on logical query maker

--- SA_SearchEngine/query_makers.py
+++ SA_SearchEngine/query_makers.py
@@ -10,3 +10,22 @@ class QueryMaker(ABC):
 class DefaultQueryMaker(QueryMaker):
     def make_query(self, search_key):
         return {'$or': list(map(lambda term: {'lookupKey': term}, search_key.split(' ')))}
+
+
+class LogicalQueryMaker(QueryMaker):
+    def make_query(self, search_key):
+        # TODO
+        result = []
+        split = search_key.split(' ')
+        for i in range(len(split)):
+            word = split[i]
+            word_before = split[i - 1]
+            if i > 0 and word_before == 'not':
+                pass
+            elif i < len(split) - 1:
+                word_next = split[i + 1]
+                if word == 'and':
+                    pass
+                elif word == 'or':
+                    pass
+        return result
