f77e71a8200d487b7db397fd27d29d04074005e2
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-26 12:31:47 -0600

    Add logical query maker

--- SA_SearchEngine/query_makers.py
+++ SA_SearchEngine/query_makers.py
@@ -14,18 +14,43 @@ class DefaultQueryMaker(QueryMaker):
 
 class LogicalQueryMaker(QueryMaker):
     def make_query(self, search_key):
-        # TODO
         result = []
+        relation_for_index = {}
         split = search_key.split(' ')
-        for i in range(len(split)):
+        for i in range(1, len(split)):
             word = split[i]
             word_before = split[i - 1]
-            if i > 0 and word_before == 'not':
-                pass
-            elif i < len(split) - 1:
-                word_next = split[i + 1]
-                if word == 'and':
-                    pass
-                elif word == 'or':
-                    pass
-        return result
+            if i > 0 and word_before.lower() == 'not':
+                relation = {'$not': [{'lookupKey': word}]}
+                relation_for_index[i] = relation
+                result.append(relation)
+        for i in range(1, len(split) - 1):
+            word_before = split[i - 1]
+            word = split[i]
+            word_next = split[i + 1]
+            if word.lower() == 'and' and i - 1 in relation_for_index:
+                prev_relation = relation_for_index[i - 1]
+                relation = {'$and': [prev_relation, {'lookupKey': word_next}]}
+                result.remove(prev_relation)
+                result.append(relation)
+            elif word.lower() == 'and' and i - 1 not in relation_for_index:
+                relation = {'$and': [{'lookupKey': word_before}, {'lookupKey': word_next}]}
+                relation_for_index[i - 1] = relation
+                result.append(relation)
+            elif word.lower() == 'or' and i - 1 in relation_for_index:
+                prev_relation = relation_for_index[i - 1]
+                relation = {'$or': [prev_relation, {'lookupKey': word_next}]}
+                result.remove(prev_relation)
+                result.append(relation)
+            elif word.lower() == 'or' and i - 1 not in relation_for_index:
+                relation = {'$or': [{'lookupKey': word_before}, {'lookupKey': word_next}]}
+                relation_for_index[i - 1] = relation
+                result.append(relation)
+            elif word_before.lower() != 'not' and word_before.lower() != 'and' and word_before.lower() != 'or':
+                result.append({'lookupKey': word})
+        if len(split) > 2:
+            if len(split) - 3 not in relation_for_index:
+                result.append({'lookupKey': split[-1]})
+        else:
+            result.append({'lookupKey': split[-1]})
+        return {'$or': result}
